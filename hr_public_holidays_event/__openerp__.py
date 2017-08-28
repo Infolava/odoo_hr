a80e5c452892a6815dd2d077fa7a96df934a7cd4
Amira Trabelsi <at@transprog.org>
2017-08-28 11:52:17 +0100

    [IMP] Add dependency to calendar addon

--- hr_public_holidays_event/__openerp__.py
+++ hr_public_holidays_event/__openerp__.py
@@ -29,11 +29,12 @@
 # --------------------------------------------------------------------------------
 {
     'name' : 'Official holidays In Calender Event',
-    'version' : '0.0.0',
+    'version' : '2.0.0',
     'author' : 'Infolava',
     'website': 'http://www.infolava.ch',
     'category' : 'Human Ressources Management',
-    'depends' : ['hr_public_holidays'],
+    'depends' : ['hr_public_holidays',
+                 'calendar'],
     'demo_xml' : [],
     'summary': 'Addon to display public holidays as event in odoo calender',
     'data' : [],
